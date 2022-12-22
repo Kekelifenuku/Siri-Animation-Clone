4b8e196acb23045f5e70f1b7ac95ed41d90ff60c
Fenuku kekeli <113697135+Kekelifenuku@users.noreply.github.com>
2022-12-22 00:02:36 +0000

    Add files via upload

---
+++ Siri Animation Clone/Siri Animation Clone/Siri_Animation_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Siri_Animation_CloneApp.swift
+//  Siri Animation Clone
+//
+//  Created by kekeli on 21/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Siri_Animation_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
